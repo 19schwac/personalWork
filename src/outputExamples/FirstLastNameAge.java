04844f1e742d29edad7deeec98bfc825734f27e1
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-09-13 13:17:18 -0500

    First and Last name, Age program

--- src/outputExamples/FirstLastNameAge.java
+++ src/outputExamples/FirstLastNameAge.java
@@ -1,11 +1,12 @@
 package outputExamples;
 
-public class FirstLastName {
+public class FirstLastNameAge {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Christian");
 		System.out.println("Schwartz");
+		System.out.println("17");
 	}
 
 }
