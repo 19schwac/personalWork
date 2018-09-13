b67455bc5efaf95c46e09a8637f8671b15fe354c
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-09-13 13:53:25 -0500

    Mailing Label program

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Christian Schwartz "
+				+ "\n19625 Killarney Way "
+				+ "\nBrookfield WI 53045");
+	}
+
+}
