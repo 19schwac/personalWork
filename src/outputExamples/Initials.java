2242426296e32b22ed61c0512657c15049fb3a82
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-09-13 14:12:07 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,18 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("  CCCCC           M         M"
+				+ "\n C              M   M      M"
+				+ "\nC              M     M    M"
+				+ "\nC             M       M  M"
+				+ "\nC            M         M"
+				+ "\nC            M         M"
+				+ "\n C           M         M"
+				+ "\n  CCCCC");
+		System.out.print("      M         M");
+	}
+
+}
