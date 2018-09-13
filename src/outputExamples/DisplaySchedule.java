5af3feed118b11a35bb94e0a9fd8bb83c3b6ece7
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-09-13 13:39:06 -0500

    Display Schedule program

---
+++ src/outputExamples/DisplaySchedule.java
@@ -0,0 +1,15 @@
+package outputExamples;
+
+public class DisplaySchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Christian Schwartz");
+		System.out.println("Utopian and Dystopian Lit");
+		System.out.println("Symphonic Band");
+		System.out.println("Economics");
+		System.out.println("AP Physics C");
+		System.out.println("AP Computer Science");
+	}
+
+}
