2198ca3d7c2687a05b59307ccf78a5c15055f664
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-09-20 14:12:25 -0500

    Unfinished password program

---
+++ src/inputExamples/PasswordGenerator.java
@@ -0,0 +1,19 @@
+package inputExamples;
+import java.util.Scanner;
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int number1 = 0;
+		int number2 = 0;
+		int number3 = 0;
+		System.out.println("Type in 3 single digits");
+		number1 = input.nextInt();
+		number2 = input.nextInt();
+		number3 = input.nextInt();
+		System.out.println("Your possible passwords are ");
+		
+	}
+
+}
