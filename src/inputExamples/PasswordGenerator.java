6a1dfdde53e9d30f1c154d0e361c413de626139e
19schwac <christianmontgomeryschwartz@gmail.com>
2018-09-20 20:28:54 -0500

    Finished Password program

--- src/inputExamples/PasswordGenerator.java
+++ src/inputExamples/PasswordGenerator.java
@@ -8,11 +8,19 @@ public class PasswordGenerator {
 		int number1 = 0;
 		int number2 = 0;
 		int number3 = 0;
-		System.out.println("Type in 3 single digits");
+		System.out.println("Type in a number with a single digit");
 		number1 = input.nextInt();
+		System.out.println("Type in a diffrent number with a single digit");
 		number2 = input.nextInt();
+		System.out.println("Type in one more diffrent number with just a single digit");
 		number3 = input.nextInt();
-		System.out.println("Your possible passwords are ");
+		System.out.println("Your possible passwords are \n"
+				+ number1 + + number2 + + number3 + "\n"
+				+ number1 + + number3 + + number2 + "\n"
+				+ number2 + + number1 + + number3 + "\n"
+				+ number2 + + number3 + + number2 + "\n"
+				+ number3 + + number1 + + number2 + "\n"
+				+ number3 + + number2 + + number1);
 		
 	}
 
