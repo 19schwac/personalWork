b2d2efe5e8425b33322c3553ab711da4138c80e6
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-09-21 14:43:41 -0500

    Edit Area of Wall

--- src/mathOperators/AreaOfWall.java
+++ src/mathOperators/AreaOfWall.java
@@ -5,12 +5,12 @@ public class AreaOfWall {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		int height = 8;
+		final int HEIGHT = 8;
 		int width = 0;
 		int area = 0;
 		System.out.println("What is the width of the wall in feet?");
 		width = input.nextInt();
-		area = width*height;
+		area = width*HEIGHT;
 		System.out.println("The area of the wall is " + area + " square feet");
 	}
 
