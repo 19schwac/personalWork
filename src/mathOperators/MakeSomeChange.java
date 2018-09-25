f28a58e17a65276c2e335fe77d914b23aba3f6dd
19schwac <christianmontgomeryschwartz@gmail.com>
2018-09-25 17:12:57 -0500

    Make change program

--- src/mathOperators/MakeSomeChange.java
+++ src/mathOperators/MakeSomeChange.java
@@ -6,19 +6,35 @@ public class MakeSomeChange {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		double initialValue = 0.00;
+		double newValue= 0.00;
 		int dollars = 0;
 		int quarters = 0;
 		int dimes = 0;
 		int nickels = 0;
 		int pennies = 0;
-		final double DOLLAR = 1.00;
-		final double QUARTER = 0.25;
-		final double DIME = 0.10;
-		final double NICKLE = 0.05;
-		final double PENNIES = 0.01;
+		final int DOLLAR = 100;
+		final int QUARTER = 25;
+		final int DIME = 10;
+		final int NICKLE = 5;
+		final int PENNIES = 1;
 		System.out.println("How much money do you have?");
 		initialValue = input.nextDouble();
-		dollars = (int)(initialValue/DOLLAR);
+		newValue = initialValue*100.0;
+		dollars = (int) (newValue/DOLLAR);
+		newValue = (newValue-dollars*DOLLAR);
+		quarters = (int)(newValue/QUARTER);
+		newValue = (newValue-quarters*QUARTER);
+		dimes = (int)(newValue/DIME);
+		newValue = (newValue-dimes*DIME);
+		nickels = (int)(newValue/NICKLE);
+		newValue = (newValue-nickels*NICKLE);
+		pennies = (int)(newValue/PENNIES);
+		System.out.println("You have $" + initialValue + " dollars. You change is");
+		System.out.println("Dollars: " + dollars);
+		System.out.println("Quarters: "+ quarters);
+		System.out.println("Dimes: " + dimes);
+		System.out.println("Nickels: " + nickels);
+		System.out.println("Pennies: " + pennies);
 		
 		
 	}
