a936d3a4f9ac153eb11eb4e17ea1d53ba877a0b5
19schwac <christianmontgomeryschwartz@gmail.com>
2018-09-25 21:28:03 -0500

    Phone number display and split

---
+++ src/mathOperators/PullApartPhoneNumber.java
@@ -0,0 +1,47 @@
+package mathOperators;
+import java.util.Scanner;
+public class PullApartPhoneNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int number1 = 0;
+		int number2 = 0;
+		int number3 = 0;
+		int number4 = 0;
+		int number5 = 0;
+		int number6 = 0;
+		int number7 = 0;
+		int number8 = 0;
+		int number9 = 0;
+		int number10 = 0;
+		System.out.println("What is the first digit of your phone number?");
+		number1 = input.nextInt();
+		System.out.println("What is the second digit of your phone number?");
+		number2 = input.nextInt();
+		System.out.println("What is the third digit of your phone number?");
+		number3 = input.nextInt();
+		System.out.println("What is the fourth digit of your phone number?");
+		number4 = input.nextInt();
+		System.out.println("What is the fith digit of your phone number?");
+		number5 = input.nextInt();
+		System.out.println("What is the sixth digit of your phone number?");
+		number6 = input.nextInt();
+		System.out.println("What is the seventh digit of your phone number?");
+		number7 = input.nextInt();
+		System.out.println("What is the eigth digit of your phone number?");
+		number8 = input.nextInt();
+		System.out.println("What is the ninth digit of your phone number?");
+		number9 = input.nextInt();
+		System.out.println("What is the tenth digit of your phone number?");
+		number10 = input.nextInt();
+		System.out.println("Your phone number is " + number1 + + number2 + + number3 + + number4 + + number5 + + number6 + + number7 + + number8 + + number9 + + number10);
+		System.out.println("Area Code: " + number1 + + number2 + + number3);
+		System.out.println("Middle Digits: " + number4 + + number5 + + number6);
+		System.out.println("Last Digits: " + number7 + + number8 + + number9 + + number10);
+		
+		
+		
+	}
+
+}
