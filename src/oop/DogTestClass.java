34fa6f980e7f9056889931f138d9346e2f2cf60b
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-01 14:56:51 -0500

    OOP examples plus dog constructor and test class

---
+++ src/oop/DogTestClass.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class DogTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog fido = new Dog();
+		fido.woof();
+		fido.attack();
+	}
+
+}
