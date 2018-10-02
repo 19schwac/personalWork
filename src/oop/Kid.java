84c21de27e0083703169e54f3e48e91b699292bf
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-02 14:03:25 -0500

    Created kid definition and test class

---
+++ src/oop/Kid.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Kid {
+	
+	public Kid() {
+
+
+	}
+	public void sayMama() {
+		System.out.println("Mama!");
+	}
+	public void sayFood() {
+		System.out.println("I am hungry.");
+	}
+	public void sayStink() {
+		System.out.println("I stink.");
+	}
+}
