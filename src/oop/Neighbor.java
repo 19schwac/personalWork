84ea4b0094cfac6bee6bb081dea8d49d1cc8642a
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-02 14:08:24 -0500

    Created neighbor definition and test class

---
+++ src/oop/Neighbor.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Neighbor {
+
+	public Neighbor() {
+
+
+	}
+	public void sayJoke() {
+		System.out.println("What did the police man say to his shirt? I have you under a vest!");
+	}
+	public void sayMoney() {
+		System.out.println("You want my money!");
+	}
+	public void sayConversation() {
+		System.out.println("Good morning! How are your today?");
+	}
+}
---
+++ src/oop/NeighborTestClass.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class NeighborTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Neighbor boy = new Neighbor();
+		boy.sayJoke();
+		boy.sayMoney();
+		boy.sayConversation();
+	}
+
+}
