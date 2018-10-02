883bf4cbdc253671caecec41f858373e0f793f9d
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-02 13:56:03 -0500

    Created car definition and test class

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -6,5 +6,13 @@ public class Car {
 
 
 	}
-
+	public void sayOdometer() {
+		System.out.println("Your car has traveled 50,000 miles.");
+	}
+	public void sayFlat() {
+		System.out.println("Your car tire is flat.");
+	}
+	public void sayThirsty() {
+		System.out.println("Your car is out of gas.");
+	}
 }
---
+++ src/oop/CarTestClass.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class CarTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car frank = new Car();
+		frank.sayOdometer();
+		frank.sayFlat();
+		frank.sayThirsty();
+	}
+
+}
