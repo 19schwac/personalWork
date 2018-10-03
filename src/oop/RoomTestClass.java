fa51b3152f23b01891ae8815962e3af96d645fbc
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-03 14:55:41 -0500

    Edit test class for room

--- src/oop/RoomTestClass.java
+++ src/oop/RoomTestClass.java
@@ -4,9 +4,12 @@ public class RoomTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Room frank = new Room();
-		frank.area();
-		frank.areaWithDoor();
+		Room closet = new Room();
+		Room kitchen = new Room();
+		kitchen = closet;
+		Room basement = new Room();
+		closet = basement;
+		closet.area();
 	}
 
 }
