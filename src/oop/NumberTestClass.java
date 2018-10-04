467df9b8a4b3c96f016e822816ae51c252d12997
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-04 14:00:24 -0500

    Edit numbers definition and test class

--- src/oop/NumberTestClass.java
+++ src/oop/NumberTestClass.java
@@ -6,6 +6,8 @@ public class NumberTestClass {
 		// TODO Auto-generated method stub
 		Numbers frank = new Numbers();
 		frank.sayNumber(7.5);
+		frank.sayNumberPlus2(7.5);
+		frank.saySum(1.0, 2.0);
 	}
 
 }
--- src/oop/Numbers.java
+++ src/oop/Numbers.java
@@ -7,5 +7,12 @@ public class Numbers {
 	public void sayNumber(double num){
 		System.out.println("Your number is " + num);
 	}
-	
+	public void sayNumberPlus2(double num){
+		num = num+2;
+		System.out.println("Your number +2 is " + num);
+	}
+	public void saySum(double num1, double num2){
+		double numTotal = num1+num2;
+		System.out.println("The sum of your two numbers is " + numTotal);
+	}
 }
