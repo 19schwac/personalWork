244c84387dd3656ff8db4b767c0c881c38d9b6d0
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-04 13:46:07 -0500

    Number definition and test class with parameters

---
+++ src/oop/NumberTestClass.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class NumberTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Numbers frank = new Numbers();
+		frank.sayNumber(7.5);
+	}
+
+}
---
+++ src/oop/Numbers.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Numbers {
+
+	public Numbers(){
+}
+	public void sayNumber(double num){
+		System.out.println("Your number is " + num);
+	}
+	
+}
