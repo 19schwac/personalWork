a3bf57b667b6ffcae24c476fef52d6fad38b24d6
19schwac <christianmontgomeryschwartz@gmail.com>
2018-10-11 20:11:55 -0500

    Finished shape test class

--- src/oop/ShapeTestClass.java
+++ src/oop/ShapeTestClass.java
@@ -31,6 +31,19 @@ double height = input.nextDouble();
 joe = new RightTriangle(base, height);
 System.out.println("Triangle Area: " + joe.area());
 System.out.println("Triangle Perimeter: "  + joe.perimeter());
+IsoscelesTrapezoid jack = new IsoscelesTrapezoid();
+System.out.println("What is the bottom base of the trapezoid in feet?");
+double base1 = input.nextDouble();
+System.out.println("What is the top base of the trapezoid in feet?");
+double base2 = input.nextDouble();
+System.out.println("What is the height of the trapezoid in feet?");
+double trapezoidHeight = input.nextDouble();
+System.out.println("What is the length of the leg of the trapezoid in feet?");
+double leg = input.nextDouble();
+jack = new IsoscelesTrapezoid(base1,base2,trapezoidHeight,leg);
+System.out.println("Trapezoid Area: " + jack.area());
+System.out.println("Trapedzoid Perimeter: " + jack.perimeter());
+
 
 
 	}
