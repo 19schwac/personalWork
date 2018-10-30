307bf3fde60b17d4906ea3a2babff5e787125a3e
19schwac <christianmontgomeryschwartz@gmail.com>
2018-10-29 23:45:13 -0500

    Number Output Program

---
+++ src/loops/NumberOutput.java
@@ -0,0 +1,24 @@
+package loops;
+
+public class NumberOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		int num = 0;
+		for (count = 0; count <= 5; count++) {
+			for (num = count; num <= 5; num++) {
+				if (num != count) {
+					System.out.print(num + " ");
+				} else {
+					if (num == 0) {
+
+					} else {
+
+						System.out.println(count);
+					}
+				}
+			}
+		}
+	}
+}
