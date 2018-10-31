f6d2e3b6c015a323c2df8b049dd07300f1d9eb29
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-31 14:01:45 -0500

    Finished Prime Factorization

---
+++ src/loops/PrimeFactorizationExample.java
@@ -0,0 +1,22 @@
+package loops; 
+ 
+import java.util.Scanner; 
+ 
+public class PrimeFactorizationExample { 
+ 
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub 
+		Scanner input = new Scanner(System.in); 
+		int num = 0; 
+		num = input.nextInt(); 
+		for (int count = 2; count < num; count++) { 
+			while (num % count == 0) { 
+				System.out.println(count + " "); 
+				num = num / count; 
+			} 
+		} 
+		if (num > 2) { 
+			System.out.println(num); 
+		} 
+	} 
+} 
