f1af44761ad83b0986b5eb2e270065978ba18012
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-10-31 15:06:06 -0500

    Perfect number and YEEEEEEEEEEEt

---
+++ src/loops/Yeet.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Yeet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int o = 0; o == o; o++) {
+			System.out.print("Y");
+			for (int i = 0; i < o; i++) {
+
+				System.out.print("e");
+			}
+			System.out.println("t");
+		}
+	}
+}
