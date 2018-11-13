3b45c73f9a0eab5e08621d8edd18f5454cd97a1e
Christian Schwartz <christianmontgomeryschwartz@gmail.com>
2018-11-13 14:56:37 -0600

    substring practice

--- src/strings/Activity6.java
+++ src/strings/Activity6.java
@@ -7,11 +7,11 @@ public class Activity6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-
 		System.out.println("Write a sentence.");
 		String sentence = input.nextLine();
 		for (int i = 0; i < sentence.length(); i++) {
-			System.out.println(sentence.charAt(i));
+			//System.out.println(sentence.charAt(i));
+			System.out.println(sentence.substring(i));
 		}
 	}
 }
